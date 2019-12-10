2fd02e5646f5067aae1326f4a31ee41903497293
zhoupeng <1875112921@qq.com>
2019-12-10 20:44:58 +0800

    布局ok

--- src/components/base/baseLayout.vue
+++ src/components/base/baseLayout.vue
@@ -6,62 +6,62 @@
           <div class="layout-logo"></div>
           <div class="layout-nav">
             <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>
-              Item 1
+              <Icon type="logo-yen" />
+              微信
             </MenuItem>
             <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>
-              Item 2
+              <Icon type="md-checkmark-circle" />
+              支付宝
             </MenuItem>
             <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>
-              Item 3
+              <Icon type="md-cloud-outline" />
+              云闪付
             </MenuItem>
             <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>
-              Item 4
+              <Icon type="logo-apple" />
+              Apple Pay
             </MenuItem>
           </div>
         </Menu>
       </Header>
       <Layout>
         <Sider hide-trigger :style="{background: '#fff'}">
-          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+          <Menu active-name="1-1" theme="light" width="auto" :open-names="['1']">
             <Submenu name="1">
               <template slot="title">
                 <Icon type="ios-navigate"></Icon>
-                Item 1
+                订单查询
               </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
+              <MenuItem name="1-1">实时消费监控</MenuItem>
+              <MenuItem name="1-2">消费订单查询</MenuItem>
+              <MenuItem name="1-3">退款订单查询</MenuItem>
             </Submenu>
             <Submenu name="2">
               <template slot="title">
                 <Icon type="ios-keypad"></Icon>
-                Item 2
+                结算明细
               </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
+              <MenuItem name="2-1">本周结算明细</MenuItem>
+              <MenuItem name="2-2">历史结算明细</MenuItem>
             </Submenu>
             <Submenu name="3">
               <template slot="title">
                 <Icon type="ios-analytics"></Icon>
-                Item 3
+                争议处理
               </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
+              <MenuItem name="3-1">消费订单处理</MenuItem>
+              <MenuItem name="3-2">退款订单处理</MenuItem>
             </Submenu>
           </Menu>
         </Sider>
         <Layout :style="{padding: '0 24px 24px'}">
           <Breadcrumb :style="{margin: '24px 0'}">
-            <BreadcrumbItem>Home</BreadcrumbItem>
-            <BreadcrumbItem>Components</BreadcrumbItem>
-            <BreadcrumbItem>Layout</BreadcrumbItem>
+            <BreadcrumbItem>微信支付</BreadcrumbItem>
+            <BreadcrumbItem>订单查询</BreadcrumbItem>
+            <BreadcrumbItem>实时消费监控</BreadcrumbItem>
           </Breadcrumb>
-          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-            Content
+          <Content :style="{padding: '24px', minHeight: '450px', background: '#f4f5f9'}">
+            <router-view></router-view>
           </Content>
         </Layout>
       </Layout>
@@ -70,7 +70,7 @@
 </template>
 <script>
   export default {
-
+    name:"baseLayout"
   }
 </script>
 <style scoped>
@@ -82,8 +82,8 @@
     overflow: hidden;
   }
   .layout-logo{
-    width: 100px;
-    height: 30px;
+    width: 80px;
+    height: 40px;
     background: #5b6270;
     border-radius: 3px;
     float: left;
@@ -92,7 +92,7 @@
     left: 20px;
   }
   .layout-nav{
-    width: 420px;
+    width: 60%;
     margin: 0 auto;
     margin-right: 20px;
   }
