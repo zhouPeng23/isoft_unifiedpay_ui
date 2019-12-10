9cb5916d454ede970a5fcc94718e8a350de0c3c3
zhoupeng <1875112921@qq.com>
2019-12-10 17:41:58 +0800

    规范化路由 index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,29 @@
+//==============================================开始引入组件============================================================
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '../index'
-import orderList from '../components/wechatpay/orderList'
-import baseLayout from '../components/base/baseLayout'
+const index = () => import('../index');
+const orderList  = () => import('../components/wechatpay/orderList');
+const baseLayout = () => import('../components/base/baseLayout');
+Vue.use(Router);
 
-Vue.use(Router)
-
+//==============================================引入二级模块============================================================
+//微信支付
 const wechatpayRouters = [
   {path:'orderList',component:orderList},
-]
+];
 
 
 
+
+
+
+//==============================================引入一级模块============================================================
 export default new Router({
-  mode:'history',//去掉#号
+  //去掉#号
+  mode:'history',
   routes: [
     {path: '/index',component: index},
-    {path:'/wechatpay',component:baseLayout,children:wechatpayRouters},
-    { path: '*', redirect: '/index' }   /*默认跳转路由*/
+    {path:'/wechatpay',component:baseLayout,children:wechatpayRouters},//微信支付
+    { path: '*', redirect: '/index' }
   ]
 })
